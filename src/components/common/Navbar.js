6031792a35b5e884a01458832294049a914d5338
joelito1234 <joel.alayon@anima.edu.uy>
2020-05-26 21:16:33 -0300

    dropdown is hidden and displayed when clicked

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -9,6 +9,12 @@ import { Menu, Close } from "@material-ui/icons";
 import Sidebar from "./Sidebar";
 
 const Navbar = ({ currentUser, logout }) => {
+  // Hide and show a options dropdown
+  const [showMe, setShowMe] = useState(false);
+  function toggle(){
+    setShowMe(!showMe);
+  }
+  // 
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
 
@@ -77,6 +83,29 @@ const Navbar = ({ currentUser, logout }) => {
                   <Link route="/">
                     <NavItem>Ranking</NavItem>
                   </Link>
+                </div>
+              </NavItems>
+              <NavItems>
+                <div class="relative inline-block text-left">
+                  <div>
+                    <span class="rounded-md shadow-sm">
+                      <button onClick={toggle} type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150">
+                        Options
+                        <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/>
+                        </svg>
+                      </button>
+                    </span>
+                  </div>
+                  <div style={{display: showMe?"block":"none"}} class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+                    <div class="rounded-md bg-white shadow-xs">
+                      <div class="py-1">
+                      <button type="submit" class="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
+                        Más reciente
+                      </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </NavItems>
             </NavContent>
