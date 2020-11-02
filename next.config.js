c54ac0eedc2451be763ad4555faefe14d8f58480
Franco González <franco.gonzalez.burgos@gmail.com>
2020-11-02 17:19:03 -0300

    Added headers on next config

--- next.config.js
+++ next.config.js
@@ -11,8 +11,23 @@ module.exports = (phase, { defaultConfig }) => {
       },
     };
   }
+  
 
   return {
+    async headers() {
+      return [
+        {
+          // mathching all API routes
+          source: "/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    },
     env: {
       BACKEND_URL: "https://tututor-backend.herokuapp.com",
       WS_URL: "wss://tututor-backend.herokuapp.com/cable"
